63d9cc25bf01f46e9410555a1b1f06db1c087a06
Padam Sethia <padamsethia5@gmail.com>
2017-09-16 18:45:35 +0530

    Converted tabs to spaces

--- main.py
+++ main.py
@@ -11,21 +11,21 @@ app = Flask(__name__)
 
 @app.route('/sentiment/<politician>')
 def getSentiment(politician):
-	tempstring = politician.replace('-' , ' ')
-	print tempstring
-	pos , neg = stream_tweets.getSentiment(tempstring)
-	return jsonify({'data' : {'pos' : pos , 'neg' :neg} ,
-					'name' : tempstring})
+    tempstring = politician.replace('-' , ' ')
+    print tempstring
+    pos , neg = stream_tweets.getSentiment(tempstring)
+    return jsonify({'data' : {'pos' : pos , 'neg' :neg} ,
+                    'name' : tempstring})
 
 @app.route('/recognize/<base64_data>')
 def getRecog(base64_data):
-	img_data = base64_data
-	with open("./image_test.jpeg", "wb") as fh:
-    	fh.write(base64.decodebytes(img_data))
+    img_data = base64_data
+    with open("./image_test.jpeg", "wb") as fh:
+        fh.write(base64.decodebytes(img_data))
     path = os.path.abspath("./image_test.jpeg")
     name = recognize.predict_face(path)
     print name 
-	return jsonify({'name' : name})    
+    return jsonify({'name' : name})    
 
 if __name__ =="__main__":
-	app.run(port=5050,debug=True)
+    app.run(port=5050,debug=True)
--- stream_tweets.py
+++ stream_tweets.py
@@ -18,22 +18,22 @@ indicoio.config.api_key = 'ef0a7214e6de676cd0c531587165ba35'
 auth = OAuthHandler(con_key , con_secret)
 auth.set_access_token(acc_token , acc_secret)
 api = tweepy.API(auth , wait_on_rate_limit = True,
-			wait_on_rate_limit_notify=True)
+            wait_on_rate_limit_notify=True)
 
 if (not api):
-	print ("Authentication Error : Unable to authenticate")
-	sys.exit(-1)
+    print ("Authentication Error : Unable to authenticate")
+    sys.exit(-1)
 
 def getSentiment(politic):
-	search_results = api.search(q=politic , count=100)
+    search_results = api.search(q=politic , count=100)
 
-	result_tweets = []
+    result_tweets = []
 
-	for result in search_results:
-		result = result.text.encode('ascii' ,errors='ignore')
-		result_tweets.append(result)
+    for result in search_results:
+        result = result.text.encode('ascii' ,errors='ignore')
+        result_tweets.append(result)
 
-	sentiment = indicoio.sentiment(result_tweets)
-	pos = int(100*(sum(sentiment)/len(sentiment)))
-	neg = int(100-pos)
-	return pos,neg
+    sentiment = indicoio.sentiment(result_tweets)
+    pos = int(100*(sum(sentiment)/len(sentiment)))
+    neg = int(100-pos)
+    return pos,neg
