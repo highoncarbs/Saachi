e59fc0d47c147cc0b20206ccbd11a70b5e52f618
Padam Sethia <padamsethia5@gmail.com>
2017-09-30 16:15:26 +0530

    Updated API

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
+
+
 '''
-This Package runs the Saachi API 
+This Package runs the Saachi API
 '''
 from flask import Flask , jsonify , request
 import stream_tweets
-import base64 
+import base64
 import recognize
-import os 
+import os
 import uuid
+from werkzeug.datastructures import ImmutableMultiDict
+
+from elasticsearch import Elasticsearch
+from json import dumps
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 UPLOAD_FOLDER = './image/'
 
+ES = Elasticsearch()
 @app.route('/sentiment/<politician>')
 def getSentiment(politician):
     tempstring = politician.replace('-' , ' ')
@@ -19,17 +28,56 @@ def getSentiment(politician):
     return jsonify({'data' : {'pos' : pos , 'neg' :neg} ,
                     'name' : tempstring})
 
-@app.route('/recognize/' , methods=['GET','POST'])
+@app.route('/recognize' , methods=['GET','POST'])
 def getRecog():
-    if request.method == 'POST':
+    if request.method == "POST":
         file = request.files['file']
-        ex = os.path.split(file.filename)[1]
+        # ex = request.form['file']
+        print file
         fname = "recog_img.jpg"
         file_path = os.path.join(UPLOAD_FOLDER , fname)
         file.save(file_path)
-        
         face = recognize.predict_face(file_path)
+        # print face
         return jsonify({'name': face})
+    else:
+        return jsonify({'name': "nannana test"})
+
+@app.route('/search/<politician>')
+def findPolitician(politician):
+    size = request.args['size']
+    search_query = {
+        'size': size,
+        'query': {
+            'dis_max': {
+                'queries': [
+                    {
+                        'match': {
+                            'name': {
+                                'query': politician,
+                                'boost': 2
+                            }
+                        }
+                    },
+                    {
+                        'match': {
+                            'S/o|D/o|W/o': {
+                                'query': politician,
+                                'boost': 1
+                            }
+                        }
+                    }
+                ]
+            }
+        },
+        'sort': [
+            {'_score': {'order': 'desc'}}
+        ]
+    }
+
+    results = ES.search(index='hackocracy', doc_type='document',
+                        body=dumps(search_query), sort='_score')
+    return jsonify(results)
 
 if __name__ =="__main__":
     app.run(port=5000,debug=False)
